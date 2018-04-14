f2ba2e57f7e3ace24cb32b55855a385eae4a84df
Stasszek <staszek906@o2.pl>
2018-04-14 13:28:42 +0200

    140418

---
+++ FrieManGame/Assets/Scrypt/BulletMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed, ForceMode.Impulse);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.tag);
+        if (other.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ FrieManGame/Assets/Scrypt/PlayerShot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShot : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float timeBettwenShooting;
+    private float lastShoot;
+     
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool inputShoot = Input.GetButton("Fire1");
+        if (inputShoot && lastShoot <= Time.time)
+        {
+            lastShoot = Time.time + timeBettwenShooting;
+            Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
