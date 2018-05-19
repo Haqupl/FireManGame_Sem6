47d4ddd65a490f0dd9a4618607b02d4b53df6766
Stasszek <staszek906@o2.pl>
2018-05-19 13:35:14 +0200

    190518

---
+++ FrieManGame/Assets/Scrypt/GameMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+    public Canvas menu;
+    private GameObject popupExitQuest;
+
+	// Use this for initialization
+	void Start () {
+        popupExitQuest = menu.transform.GetChild(2).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame(int indexScene)
+    {
+        menu.gameObject.SetActive(false);
+        SceneManager.LoadScene(indexScene);
+    }
+
+    public void ExitGameQuest()
+    {
+        popupExitQuest.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ExitPopupMenuQuest(){
+        popupExitQuest.SetActive(false);
+    }
+
+
+
+}
---
+++ FrieManGame/Assets/Scrypt/NotDestroyObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NotDestroyObject : MonoBehaviour
+{
+    public GameObject[] NotDestroy;
+   
+    void Start()
+    {
+        for (int i = 0; i < NotDestroy.Length; i++)
+        {
+            DontDestroyOnLoad(NotDestroy[i]);
+        }
+    } 
+}
